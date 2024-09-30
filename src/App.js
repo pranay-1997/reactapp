2c5ed9b7c618b4717c121d76496eea2091320abf
pranay <pranaykdohni@gmail.com>
2024-09-30 14:07:52 +0530

    added Message file in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import App1 from './components/App1';
 import App2 from './components/App2';
+import Message from './components/Message';
 
 function App() {
+  //props are immutable
   return (
     <div className="App">
       <App1 name="pranay" heroName="chinna"/>
       <App2/>
+      <Message/>
     </div>
   );
 }
